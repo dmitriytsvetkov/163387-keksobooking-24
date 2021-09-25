a4dc4e0acdca0b5c7d5582afaeceb6df59bf2ce3
Tsvetkov HomePC <dstvetkov@gmail.com>
2021-09-25 17:57:43 +0300

    добавил функцию генерации целых чисел

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-const randomFloatBetween = (minValue,maxValue,precision) => {
-  if (minValue || maxValue < 0) {
-    return;
+const getRandomInteger = (minValue, maxValue) => {
+  if (minValue < 0 || maxValue < 0) {
+    throw new Error('Min or Max value must be greater or equals to 0');
   }
   if (maxValue <= minValue) {
-    return;
+    throw new Error('Min should be less than max');
+  }
+  return Math.floor(minValue + Math.random() * (maxValue + 1 - minValue));
+};
+
+const getRandomFloat = (minValue, maxValue, precision) => {
+  if (minValue < 0 || maxValue < 0) {
+    throw new Error('Min or Max value must be greater or equals to 0');
+  }
+  if (maxValue <= minValue) {
+    throw new Error('Min should be less than max');
   }
   return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)),maxValue).toFixed(precision));
 };
 
-randomFloatBetween(0, 1, 2);
+getRandomInteger(0, 5);
+getRandomFloat(0, 1, 2);
