112c08f711b2b67ff2a74982d6f10af421c7fbf2
Tsvetkov HomePC <dstvetkov@gmail.com>
2021-10-16 13:26:28 +0300

    сделал задание 2 модуля 7

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import {createPopup} from './popup.js';
 import {createOffers} from './mock/offers.js';
+import {disableForms, enableForms} from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 
 const offers = createOffers();
 const popupElement = createPopup(offers[0]);
 mapCanvas.appendChild(popupElement);
+
+disableForms();
+enableForms();
