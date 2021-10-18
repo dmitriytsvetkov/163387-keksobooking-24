f30b59e64811fe08c98c85f35ae0ec998fdbf351
Tsvetkov HomePC <dstvetkov@gmail.com>
2021-10-18 23:00:15 +0300

    добавил валидацию 3 полей формы

--- js/form.js
+++ js/form.js
@@ -1,4 +1,26 @@
+const form = document.querySelector('.ad-form');
+const formRoomNumberSelect = form.querySelector('#room_number');
+const formCapacitySelect = form.querySelector('#capacity');
+const formCapacityOptions = Array.from(formCapacitySelect.options);
 const disabledFormClass = 'ad-form--disabled';
+
+const conditions = {
+  1 : [
+    '1',
+  ],
+  2 : [
+    '1',
+    '2',
+  ],
+  3 : [
+    '1',
+    '2',
+    '3',
+  ],
+  100 : [
+    '0',
+  ],
+};
 
 const disableInteractiveElements = (rootSelector, childrenSelector) => {
   const rootElement = document.querySelector(rootSelector);
@@ -14,7 +36,7 @@ const enableInteractiveElements = (rootSelector, childrenSelector) => {
   rootElement.classList.remove(disabledFormClass);
   const childrenElements = rootElement.querySelectorAll(childrenSelector);
   childrenElements.forEach((element) => {
-    element.disabled = true;
+    element.disabled = false;
   });
 };
 
@@ -27,5 +49,27 @@ const enableForms = () => {
   enableInteractiveElements('.ad-form', 'fieldset');
   enableInteractiveElements('.map__filters', 'fieldset, fieldset>input, select');
 };
+
+const disablePlacesForRooms = (rooms) => {
+  const result = conditions[rooms];
+  formCapacityOptions.forEach((element) => {
+    element.disabled = !result.includes(element.value);
+  });
+};
+
+const selectInitialOption = (rooms) => {
+  const result = conditions[rooms];
+  if (!result.includes(formCapacitySelect.value)) {
+    formCapacitySelect.value = result[0];
+  }
+};
+
+formRoomNumberSelect.addEventListener('change', (evt) => {
+  disablePlacesForRooms(evt.target.value);
+  selectInitialOption(evt.target.value);
+});
+
+disablePlacesForRooms(formRoomNumberSelect.value);
+selectInitialOption(formRoomNumberSelect.value);
 
 export {disableForms, enableForms};
